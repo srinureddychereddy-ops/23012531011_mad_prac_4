bc7f7a4afc42b0f2f6b9a162c52889ca2f7b49ff
srinureddychereddy <srinureddychereddy@gmail.com>
2025-11-22 22:51:13 +0530

    prac 4

---
+++ app/src/main/java/com/srinivasreddy_23012531011/a23012531011_mad_prac4/AlarmService.kt
@@ -0,0 +1,31 @@
+package com.srinivasreddy_23012531011.a23012531011_mad_prac4
+
+import android.app.Service
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.IBinder
+
+
+class AlarmService : Service() {
+
+    lateinit var mediaPlayer: MediaPlayer
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (!this::mediaPlayer.isInitialized){
+            mediaPlayer = MediaPlayer.create(this,R.raw.alarm)
+        }
+        mediaPlayer.start()
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        if (this :: mediaPlayer.isInitialized){
+            mediaPlayer.stop()
+            mediaPlayer.release()
+        }
+        super.onDestroy()
+    }
+}
